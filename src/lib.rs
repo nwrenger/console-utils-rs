b08f7fa31f8c08ff786fceb404e00e2d31cb4ae5
nwrenger <nils@wrenger.net>
2023-12-10 18:11:30 +0100

    :wrench: Fixed Dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! console-utils = "1.5.8"
+//! console-utils = "1.5.9"
 //! ```
 //!
 //! After adding the dependency, you can import the modules you need in your Rust code. For example:
